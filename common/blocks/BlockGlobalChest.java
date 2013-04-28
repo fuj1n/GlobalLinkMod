c6f94f132c2673d9ac9139a216738d70b76c340c
Arthur Uzulin <fuj1nman@hotmail.com>
2013-04-29 04:07:34 +1000

    Fixed a major error.

--- common/blocks/BlockGlobalChest.java
+++ common/blocks/BlockGlobalChest.java
@@ -101,20 +101,21 @@ public class BlockGlobalChest extends BlockContainer {
 	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random) {
 		if (par5Random.nextInt(2) == 0) {
 			for (int l = 0; l < 3; ++l) {
-				double d1 = (par3 + par5Random.nextFloat());
-				(par4 + par5Random.nextFloat());
-				double d2 = 0.0D;
-				double d3 = 0.0D;
-				double d4 = 0.0D;
-				int i1 = par5Random.nextInt(2) * 2 - 1;
-				int j1 = par5Random.nextInt(2) * 2 - 1;
-				d2 = (par5Random.nextFloat() - 0.5D) * 0.125D;
-				d3 = (par5Random.nextFloat() - 0.5D) * 0.125D;
-				d4 = (par5Random.nextFloat() - 0.5D) * 0.125D;
-				double d5 = par4 + 0.5D + 0.25D * j1;
-				d4 = (par5Random.nextFloat() * 1.0F * j1);
-				double d6 = par2 + 0.5D + 0.25D * i1;
-				d2 = (par5Random.nextFloat() * 1.0F * i1);
+				double d0 = (double)((float)par2 + par5Random.nextFloat());
+	            double d1 = (double)((float)par3 + par5Random.nextFloat());
+	            d0 = (double)((float)par4 + par5Random.nextFloat());
+	            double d2 = 0.0D;
+	            double d3 = 0.0D;
+	            double d4 = 0.0D;
+	            int i1 = par5Random.nextInt(2) * 2 - 1;
+	            int j1 = par5Random.nextInt(2) * 2 - 1;
+	            d2 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
+	            d3 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
+	            d4 = ((double)par5Random.nextFloat() - 0.5D) * 0.125D;
+	            double d5 = (double)par4 + 0.5D + 0.25D * (double)j1;
+	            d4 = (double)(par5Random.nextFloat() * 1.0F * (float)j1);
+	            double d6 = (double)par2 + 0.5D + 0.25D * (double)i1;
+	            d2 = (double)(par5Random.nextFloat() * 1.0F * (float)i1);
 				par1World.spawnParticle("enchantmenttable", d6, d1, d5, d2, d3, d4);
 			}
 		}
