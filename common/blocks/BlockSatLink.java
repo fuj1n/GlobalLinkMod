b6f7357f0fb5da944332863dde63b7efcff8a1fd
Arthur Uzulin <fuj1nman@hotmail.com>
2013-05-05 16:23:44 +1000

    Satlink blocks now textured like iron blocks

--- common/blocks/BlockSatLink.java
+++ common/blocks/BlockSatLink.java
@@ -2,16 +2,17 @@ package fuj1n.globalChestMod.common.blocks;
 
 import java.util.List;
 
-import fuj1n.globalChestMod.client.ClientProxyGlobalChests;
-import fuj1n.globalChestMod.common.tileentity.TileEntitySatLink;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import fuj1n.globalChestMod.client.ClientProxyGlobalChests;
+import fuj1n.globalChestMod.common.tileentity.TileEntitySatLink;
 
 public class BlockSatLink extends BlockMultiBlock{
 
@@ -29,10 +30,26 @@ public class BlockSatLink extends BlockMultiBlock{
 		return false;
 	}
 	
+	//@Override
+	//public Icon getIcon(int par1, int par2){
+	//	return Block.blockIron.getIcon(par1, par2);
+	//}
+	
+	//@Override
+	//public void registerIcons(IconRegister par1IconRegister){}
+	
 	@Override
 	public int getRenderType(){
 		return ClientProxyGlobalChests.SatLinkRenderId;
 	}
+	
+	@Override
+    public AxisAlignedBB getSelectedBoundingBoxFromPool(World par1World, int par2, int par3, int par4){
+		if(par1World.getBlockMetadata(par2, par3, par4) == 1){
+			return AxisAlignedBB.getAABBPool().getAABB((double)par2 + this.minX, (double)par3 + this.minY, (double)par4 + this.minZ, (double)par2 + this.maxX, (double)par3 + 0.1F, (double)par4 + this.maxZ);
+		}
+        return AxisAlignedBB.getAABBPool().getAABB((double)par2 + this.minX, (double)par3 + this.minY, (double)par4 + this.minZ, (double)par2 + this.maxX, (double)par3 + this.maxY, (double)par4 + this.maxZ);
+    }
 	
 	@Override
 	public boolean isBlockValidMultiBlockController(World par1World, int par2, int par3, int par4) {
@@ -50,10 +67,14 @@ public class BlockSatLink extends BlockMultiBlock{
 		}
 		return false;
 	}
-
+	
 	@Override
 	public void formMultiBlock(World par1World, int par2, int par3, int par4) {
-		TileEntity te = par1World.getBlockTileEntity(par2, par3, par4);
+		TileEntitySatLink te = (TileEntitySatLink)par1World.getBlockTileEntity(par2, par3, par4);
+		if(te == null){
+			te = (TileEntitySatLink)this.createNewTileEntity(par1World);
+		}
+		te.isMultiblockFormed = true;
 		par1World.setBlockMetadataWithNotify(par2, par3, par4, 1, 2);
 		par1World.setBlockTileEntity(par2, par3, par4, te);
 		par1World.setBlockMetadataWithNotify(par2, par3 + 1, par4, 2, 2);
@@ -63,7 +84,11 @@ public class BlockSatLink extends BlockMultiBlock{
 	
 	@Override
 	public void breakMultiBlock(World par1World, int par2, int par3, int par4) {
-		TileEntity te = par1World.getBlockTileEntity(par2, par3, par4);
+		TileEntitySatLink te = (TileEntitySatLink)par1World.getBlockTileEntity(par2, par3, par4);
+		if(te == null){
+			te = (TileEntitySatLink)this.createNewTileEntity(par1World);
+		}
+		te.isMultiblockFormed = false;
 		int block1 = par1World.getBlockId(par2, par3, par4);
 		int block2 = par1World.getBlockId(par2, par3 + 1, par4);
 		int meta1 = par1World.getBlockMetadata(par2, par3, par4);
@@ -98,7 +123,7 @@ public class BlockSatLink extends BlockMultiBlock{
 			this.setBlockBounds(0.4F, 0.0F, 0.4F, 0.6F, 1.0F, 0.6F);
 			break;
 		case 1:
-			this.setBlockBounds(0.4F, 0.0F, 0.4F, 0.6F, 1.0F, 0.6F);
+			this.setBlockBounds(0.0F, 0.0F, 0.0F, 1F, 1.0F, 1F);
 			break;
 		case 2:
 			this.setBlockBounds(0.4F, 0.0F, 0.4F, 0.6F, 0.6F, 0.6F);
@@ -114,13 +139,16 @@ public class BlockSatLink extends BlockMultiBlock{
 	}
 	
 	@Override
-    public void addCollisionBoxesToList(World par1World, int par2, int par3, int par4, AxisAlignedBB par5AxisAlignedBB, List par6List, Entity par7Entity)
-    {
-        this.setBlockBoundsBasedOnState(par1World, par2, par3, par4);
+    public void addCollisionBoxesToList(World par1World, int par2, int par3, int par4, AxisAlignedBB par5AxisAlignedBB, List par6List, Entity par7Entity){
+        if(par1World.getBlockMetadata(par2, par3, par4) == 1){
+        	this.setBlockBounds(0.4F, 0.0F, 0.4F, 0.6F, 1.0F, 0.6F);
+        }else{
+        	this.setBlockBoundsBasedOnState(par1World, par2, par3, par4);
+        }
         super.addCollisionBoxesToList(par1World, par2, par3, par4, par5AxisAlignedBB, par6List, par7Entity);
         int meta = par1World.getBlockMetadata(par2, par3, par4);
         if(meta == 1){
-        	this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.2F, 1.0F);
+        	this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.1F, 1.0F);
         	super.addCollisionBoxesToList(par1World, par2, par3, par4, par5AxisAlignedBB, par6List, par7Entity);
         }
         this.setBlockBoundsForItemRender();
--- common/tileentity/TileEntitySatLink.java
+++ common/tileentity/TileEntitySatLink.java
@@ -1,6 +1,7 @@
 package fuj1n.globalChestMod.common.tileentity;
 
 import net.minecraft.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
@@ -10,14 +11,20 @@ public class TileEntitySatLink extends TileEntity{
 	public float topBitRotationAngle = 0F;
 	
 	@Override
-    public AxisAlignedBB getRenderBoundingBox()
-    {
-		AxisAlignedBB bb = AxisAlignedBB.getAABBPool().getAABB(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 2, zCoord + 1);
-        return bb;
+	public void readFromNBT(NBTTagCompound par1NBTTagCompound){
+        super.readFromNBT(par1NBTTagCompound);
+        boolean valid = par1NBTTagCompound.hasKey("formed") ? par1NBTTagCompound.getBoolean("formed") : false;
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound par1NBTTagCompound){
+    	super.writeToNBT(par1NBTTagCompound);
+    	par1NBTTagCompound.setBoolean("formed", isMultiblockFormed);
     }
 	
 	@Override
-    public boolean isInvalid(){
-        return !isMultiblockFormed;
+    public AxisAlignedBB getRenderBoundingBox(){
+		AxisAlignedBB bb = AxisAlignedBB.getAABBPool().getAABB(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 2, zCoord + 1);
+        return bb;
     }
 }
