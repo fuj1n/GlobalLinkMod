4a609274c830374f9c5ad070e6493791f02943d3
Arthur Uzulin <fuj1nman@hotmail.com>
2013-05-08 20:46:43 +1000

    Added weights for BuildCraft Transport.

--- common/modcompat/CompatModuleBC.java
+++ common/modcompat/CompatModuleBC.java
@@ -3,6 +3,7 @@ package fuj1n.globalChestMod.common.modcompat;
 import buildcraft.BuildCraftBuilders;
 import buildcraft.BuildCraftCore;
 import buildcraft.BuildCraftEnergy;
+import buildcraft.BuildCraftTransport;
 import cpw.mods.fml.common.Loader;
 import fuj1n.globalChestMod.GlobalChests;
 import fuj1n.globalChestMod.lib.ManagerGlobalChest;
@@ -83,6 +84,7 @@ public class CompatModuleBC extends CompatModule{
 		me.addItemToList(BuildCraftBuilders.builderBlock.blockID, 728);
 		me.addItemToList(BuildCraftBuilders.architectBlock.blockID, 729);
 		me.addItemToList(BuildCraftBuilders.libraryBlock.blockID, 73);
+			//TRANSPORT
 		//Items
 			//CORE
 		me.addItemToList(BuildCraftCore.woodenGearItem.itemID, 2);
@@ -97,6 +99,37 @@ public class CompatModuleBC extends CompatModule{
 			//BUILDERS
 		me.addItemToList(BuildCraftBuilders.templateItem.itemID, 9);
 		me.addItemToList(BuildCraftBuilders.blueprintItem.itemID, 9);
+			//TRANSPORT
+		me.addItemToList(BuildCraftTransport.pipeWaterproof.itemID, 1);
+		me.addItemToList(BuildCraftTransport.pipeGate.itemID, 24);
+		me.addItemToList(BuildCraftTransport.pipeGateAutarchic.itemID, 62);
+		me.addItemToList(BuildCraftTransport.redPipeWire.itemID, 11);
+		me.addItemToList(BuildCraftTransport.bluePipeWire.itemID, 11);
+		me.addItemToList(BuildCraftTransport.greenPipeWire.itemID, 11);
+		me.addItemToList(BuildCraftTransport.yellowPipeWire.itemID, 11);
+		me.addItemToList(BuildCraftTransport.pipeItemsWood.itemID, 3);
+		me.addItemToList(BuildCraftTransport.pipeItemsEmerald.itemID, 33);
+		me.addItemToList(BuildCraftTransport.pipeItemsStone.itemID, 3);
+		me.addItemToList(BuildCraftTransport.pipeItemsCobblestone.itemID, 3);
+		me.addItemToList(BuildCraftTransport.pipeItemsIron.itemID, 19);
+		me.addItemToList(BuildCraftTransport.pipeItemsGold.itemID, 25);
+		me.addItemToList(BuildCraftTransport.pipeItemsDiamond.itemID, 129);
+		me.addItemToList(BuildCraftTransport.pipeItemsObsidian.itemID, 3);
+		me.addItemToList(BuildCraftTransport.pipeItemsVoid.itemID, 3);
+		me.addItemToList(BuildCraftTransport.pipeItemsSandstone.itemID, 9);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsWood.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsCobblestone.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsStone.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsIron.itemID, 20);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsGold.itemID, 26);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsVoid.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsSandstone.itemID, 10);
+		me.addItemToList(BuildCraftTransport.pipeLiquidsEmerald.itemID, 34);
+		me.addItemToList(BuildCraftTransport.pipePowerWood.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipePowerStone.itemID, 4);
+		me.addItemToList(BuildCraftTransport.pipePowerGold.itemID, 26);
+		me.addItemToList(BuildCraftTransport.facadeItem.itemID, 7);
+		me.addItemToList(BuildCraftTransport.pipeStructureCobblestone.itemID, 4);
 	}
 	
 }
